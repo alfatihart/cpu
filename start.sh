c8b35f74c80b0a0aa92f894fbab86d8415430a62
Al-Fatih Art <restu6005@gmail.com>
2021-03-14 06:42:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://yescrypt.sea.mine.zpool.ca:6233 -u DGNUnmNcxhmqQB2rudPNzC8XS9WFc3vmqg -p c=DOG
 
 
 done
